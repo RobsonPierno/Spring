65a59524f91497db4ebaa87de715fc5ffdbdb17b
Robson Pierno de Jesus <robson.pierno@gmail.com>
2018-09-25 21:16:00 -0300

    Including price list into form

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -2,6 +2,7 @@ package br.com.casadocodigo.loja.models;
 
 import java.util.List;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Produto {
 	private String descricao;
 	private int paginas;
 	
+	@ElementCollection
 	private List<Preco> precos;
 	
 	public int getId() {
